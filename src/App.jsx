75dfcd458e3b84e4b4de8f04c040092836a7701d
Marvel <dreelceejay79@yahoo.com>
2024-02-21 13:51:29 +0100

    react first class

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Board from './Square.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Board/>
+    
+    
+    </>
+  )
+}
+
+export default App
